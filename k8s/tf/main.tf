7b61bfbf0da379e848de1638d86cf47ee544a7dd
Josh Lee <josh@joshuamlee.com>
2024-08-13 15:56:23 -0400

    adds terraform example with direct resource

---
+++ k8s/tf/main.tf
@@ -0,0 +1,41 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+} 
+
+resource "kubernetes_namespace" "clickhouse" {
+  metadata {
+    name = "clickhouse"
+  }
+}
+
+resource "helm_release" "altinity_operator" {
+  name       = "altinity-operator"
+  repository = "https://altinity.github.io/clickhouse-operator"
+  chart      = "altinity-clickhouse-operator"
+  namespace  = "kube-system"
+  version    = "0.23.6"
+}
+
+resource "kubernetes_manifest" "clickhouse_installation" {
+  manifest = {
+    "apiVersion" = "clickhouse.altinity.com/v1"
+    "kind" = "ClickHouseInstallation"
+    "metadata" = {
+      "name" = "simple"
+      "namespace" = kubernetes_namespace.clickhouse.metadata.0.name
+    }
+    "spec" = {
+      "configuration" = {
+        "clusters" = [
+          {name = "simple"}
+        ]
+      }
+    }
+  }
+}
